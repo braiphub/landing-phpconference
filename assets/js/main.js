0ece865cd710fba9f2b9f3b78aae6301bc81f907
Wesley Gomes <wesleytsst@outlook.com>
2023-12-18 11:00:24 -0300

    feat: section about

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,19 @@ carousel.addEventListener("mouseenter", () => {
 carousel.addEventListener("mouseleave", () => {
   wordsContainer.style.animationPlayState = "running";
 });
+
+// Função para abrir o modal e reproduzir o vídeo
+var videoLink = "https://www.youtube.com/embed/5IvxzHrSFUU";
+function openModal() {
+  var modal = document.getElementById("videoModal");
+  var iframe = modal.querySelector("iframe");
+  iframe.src = videoLink;
+  modal.style.display = "block";
+}
+// Função para fechar o modal e pausar o vídeo
+function closeModal() {
+  var modal = document.getElementById("videoModal");
+  var iframe = modal.querySelector("iframe");
+  iframe.src = "";
+  modal.style.display = "none";
+}
