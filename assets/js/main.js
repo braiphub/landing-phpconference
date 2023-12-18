6d7fe7f2d4959c32e0029f8a337ece31d5205ef6
Wesley Gomes <wesleytsst@outlook.com>
2023-12-18 14:32:58 -0300

    feat: section buy

--- assets/js/main.js
+++ assets/js/main.js
@@ -111,3 +111,26 @@ for (var i = 0; i < speakers.length; i++) {
   info.appendChild(theme);
   info.appendChild(date);
 }
+
+/* CONTAGEM REGRESSIVA */
+
+function countdown() {
+  const targetDate = new Date("2024-06-06T00:13:00").getTime();
+  const now = new Date().getTime();
+  const timeDifference = targetDate - now;
+
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerText = `${days}`;
+  document.getElementById("hours").innerText = `${hours}`;
+  document.getElementById("minutes").innerText = `${minutes}`;
+  document.getElementById("seconds").innerText = `${seconds}`;
+}
+
+setInterval(countdown, 1000);
+countdown();
